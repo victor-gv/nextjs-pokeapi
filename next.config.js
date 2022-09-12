ad2457cd077041bfe4dd570d074141d092ba5118
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-09-12 13:01:04 +0200

    first commit, setup done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
