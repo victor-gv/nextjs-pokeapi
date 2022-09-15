3adeb1be70aa2be424da04dd904578633a29c3e2
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-09-15 14:08:37 +0200

    added pokemon details template

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -4,82 +4,111 @@ import { GetStaticPaths } from "next";
 interface Pokemon {
   name: string;
   id: number;
-  // abilities: {
-  //   ability: {
-  //     name: string;
-  //   };
-  // }[];
-  // stats: {
-  //   stat: {
-  //     name: string;
-  //   };
-  //   base_stat: number;
-  // }[];
-  // base_experience: number;
+  abilities: {
+    ability: {
+      name: string;
+    };
+  }[];
+  stats: {
+    stat: {
+      name: string;
+    };
+    base_stat: number;
+  }[];
+  base_experience: number;
 }
 
-const Pokemon = ({ pokemon }: { pokemon: Pokemon }) => {
-  console.log(pokemon);
+const Pokemon = (pokemon: any) => {
+  const bgColors: { [key: string]: string } = {
+    grass: "bg-grass",
+    poison: "bg-poison",
+    fire: "bg-fire",
+    flying: "bg-flying",
+    water: "bg-water",
+    bug: "bg-bug",
+    normal: "bg-normal",
+    electric: "bg-electric",
+    ground: "bg-ground",
+    fairy: "bg-fairy",
+    fighting: "bg-fighting",
+    psychic: "bg-psychic",
+    rock: "bg-rock",
+    ghost: "bg-ghost",
+    ice: "bg-ice",
+    dragon: "bg-dragon",
+    steel: "bg-steel",
+    dark: "bg-dark",
+  };
+
+  const pokemonType = pokemon.pokemon.types[0].type.name;
+  const pokemonImg =
+    pokemon.pokemon.sprites.other["official-artwork"].front_default;
+
   return (
     <>
-      <div className="bg-gray-800 min-h-screen">
-      
-      <figure className="card card--fairy">
-  <div className="card__image-container">
-    <img src="https://cdn.bulbagarden.net/upload/thumb/e/e8/700Sylveon.png/600px-700Sylveon.png" alt="Sylveon" className="card__image"/>   
-  </div>
-  
-  <figcaption className="card__caption">
-    <h1 className="card__name">Sylveon</h1>
+      <div className="flex flex-col justify-center items-center bg-gray-800 min-h-screen">
+        <figure
+          className={`card bg-
+          ${bgColors[pokemonType]}
+                  `}
+        >
+          <div className="card__image-container">
+            <img
+              src={pokemonImg}
+              alt={pokemon.pokemon.name}
+              className="card__image"
+            />
+          </div>
 
-    <h3 className="card__type">
-      fairy
-    </h3>
+          <figcaption className="card__caption">
+            <h1 className="card__name capitalize">{pokemon.pokemon.name}</h1>
 
-    <table className="card__stats">
-      <tbody><tr>
-        <th>HP</th>
-        <td>95</td>
-      </tr>
-      <tr>
-        <th>Attack</th>
-        <td>65</td>
-      </tr>
-      
-      <tr>
-        <th>Defense</th>
-        <td>65</td>
-      </tr>
+            <h3 className="card__type">{pokemonType}</h3>
 
-      <tr>
-        <th>Special Attack</th>
-        <td>110</td>
-      </tr>
-      <tr>
-        <th>Special Defense</th>
-        <td>130</td>
-      </tr>
-      <tr>
-        <th>Speed</th>  
-        <td>60</td>
-      </tr>
-    </tbody></table>
-    
-    <div className="card__abilities">
-      <h4 className="card__ability">
-        <span className="card__label">Ability</span>
-        Cute Charm
-      </h4>
-      <h4 className="card__ability">
-        <span className="card__label">Hidden Ability</span>
-        Pixilate
-      </h4>
-    </div>
-  </figcaption>
+            <table className="card__stats">
+              <tbody>
+                <tr>
+                  <th>HP</th>
+                  <td>95</td>
+                </tr>
+                <tr>
+                  <th>Attack</th>
+                  <td>65</td>
+                </tr>
+
+                <tr>
+                  <th>Defense</th>
+                  <td>65</td>
+                </tr>
+
+                <tr>
+                  <th>Special Attack</th>
+                  <td>110</td>
+                </tr>
+                <tr>
+                  <th>Special Defense</th>
+                  <td>130</td>
+                </tr>
+                <tr>
+                  <th>Speed</th>
+                  <td>60</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="card__abilities">
+              <h4 className="card__ability">
+                <span className="card__label">Ability</span>
+                Cute Charm
+              </h4>
+              <h4 className="card__ability">
+                <span className="card__label">Hidden Ability</span>
+                Pixilate
+              </h4>
+            </div>
+          </figcaption>
         </figure>
-        
       </div>
-
     </>
   );
 };
